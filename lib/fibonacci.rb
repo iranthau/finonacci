4c76f2b76a1ce1e3efee74db089b9b0aab710525
Iran <iranthau@gmail.com>
2016-06-01 10:55:28 +1000

    Fibonocci using a recursive function

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -1,11 +1,16 @@
+
+# Fibonacci
 module Fibonacci
   def self.fibonacci(number)
     return [0] if number == 1
     return [0, 1] if number == 2
-    fibs = [0, 1]
-    while fibs.size < number
-      fibs << fibs[-1] + fibs[-2]
-    end
-    fibs
+    sequence = [0, 1]
+    fibs(sequence, number)
+  end
+
+  def self.fibs(sequence, number)
+    return sequence if sequence.size == number
+    sequence << sequence[-1] + sequence[-2]
+    fibs(sequence, number)
   end
 end
